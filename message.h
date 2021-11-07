fd0453c9317506d115d8ad4f80416cad1b2aa1a1
Numillyah <Numillyash-std@yandex.ru>
2021-11-07 18:03:27 +0300

    Тест полного функционала, попытка 1

--- message.h
+++ message.h
@@ -6,7 +6,7 @@
 #define PICO_PWM_AUDIO_MAIN_MESSAGE_H
 
 #define msg1_len 15
-int message1[15] =
+const int message1[15] =
         {
                 -1,8,24,18,22,15,1,5,9,25,9,16,11,0,26
         };
